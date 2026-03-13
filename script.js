fc22bf696e35975fb35781f7bb214168557de472
Vdylant <95872386+Vdylant@users.noreply.github.com>
2026-03-13 14:32:54 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+{\rtf1\ansi\ansicpg1252\cocoartf2820
+\cocoatextscaling0\cocoaplatform0{\fonttbl\f0\fswiss\fcharset0 Helvetica;}
+{\colortbl;\red255\green255\blue255;}
+{\*\expandedcolortbl;;}
+\margl1440\margr1440\vieww11520\viewh8400\viewkind0
+\pard\tx720\tx1440\tx2160\tx2880\tx3600\tx4320\tx5040\tx5760\tx6480\tx7200\tx7920\tx8640\pardirnatural\partightenfactor0
+
+\f0\fs24 \cf0 // JS \uc0\u29992 \u26469 \u20197 \u21518 \u25193 \u23637 \u65292 \u27604 \u22914 \u23548 \u33322 \u39640 \u20142 \u12289 \u21160 \u30011 \u31561 \
+console.log("\uc0\u39046 \u20808 \u26410 \u26469 \u23448 \u32593 \u24050 \u21152 \u36733 ");}
